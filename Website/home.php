512cc3924583fc423e587e6ec43bf8046794026c
Rayner Brandon <101938063@student.swin.edu.au>
2021-05-16 14:12:17 +1000

    UPADATE: Fixed page links to php

--- Website/home.php
+++ Website/home.php
@@ -10,12 +10,12 @@
 </div>
 
 <div class="topnav">
-  <a class="active" href="home.html">Manager Home Screen</a>
-  <a href="inventory.html">Inventory Managemnet Screen</a>
-  <a href="sales_management.html">Sales Management Screen</a>
-  <a href="sales_analytics.html">Sales Analytics Screen</a>
-  <a href="cash_register.html">CashRegister Screen</a>
-  <a href="login.html" style="float:right">Logout</a>
+  <a href="home.php">Manager Home Screen</a>
+  <a href="inventory.php">Inventory Managemnet Screen</a>
+  <a href="sales_management.php">Sales Management Screen</a>
+  <a href="sales_analytics.php">Sales Analytics Screen</a>
+  <a class="active" href="cash_register.php">CashRegister Screen</a>
+  <a href="login.php" style="float:right">Logout</a>
 </div>
 
 <div class="row">
--- Website/sales_analytics.php
+++ Website/sales_analytics.php
@@ -12,12 +12,12 @@
 </div>
 
 <div class="topnav">
-  <a href="home.html">Manager Home Screen</a>
-  <a href="inventory.html">Inventory Managemnet Screen</a>
-  <a href="sales_management.html">Sales Management Screen</a>
-  <a class="active" href="sales_analytics.html">Sales Analytics Screen</a>
-  <a href="cash_register.html">CashRegister Screen</a>
-  <a href="login.html" style="float:right">Logout</a>
+  <a href="home.php">Manager Home Screen</a>
+  <a href="inventory.php">Inventory Managemnet Screen</a>
+  <a href="sales_management.php">Sales Management Screen</a>
+  <a href="sales_analytics.php">Sales Analytics Screen</a>
+  <a class="active" href="cash_register.php">CashRegister Screen</a>
+  <a href="login.php" style="float:right">Logout</a>
 </div>
 
 <div><h1 style="text-align: center">INVENTORY</h1></div>
